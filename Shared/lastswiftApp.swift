e3dfffcce774e4ed6040a34e15bb37c734a59daa
ghayda <ighaydas2@gmail.com>
2022-11-02 12:15:22 +0530

    Initial Commit

---
+++ Shared/lastswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  lastswiftApp.swift
+//  Shared
+//
+//  Created by mac on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct lastswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
